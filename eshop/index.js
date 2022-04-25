e39ca264cb5eb10fc812cf356ffbce7546fd475a
renan tomazini <renantomazini@gmail.com>
2022-04-25 19:52:47 -0300

    Pequena correção

--- eshop/index.js
+++ eshop/index.js
@@ -7,7 +7,7 @@ const produto1 = new Produto("Alface", 0.67);
 const produto2 = new Produto("Alçaga",0.98);
 const produto3 = new Produto("Alho poró", 1.20);
 const produto4 = new Produto("Batata", 0.78);
-const produto5 = new Produto("Paste de dentes", 2,75);
+const produto5 = new Produto("Pasta de dentes", 2,75);
 const produto6 = new Produto("Pão", 0.88);
 //console.log(produto1.nomeProduto)
 
@@ -20,5 +20,9 @@ cliente1.adicionarProdutoNoCarrinho(produto3);
 cliente1.adicionarProdutoNoCarrinho(produto4);
 cliente1.adicionarProdutoNoCarrinho(produto5);
 cliente1.adicionarProdutoNoCarrinho(produto6);
+cliente1.adicionarProdutoNoCarrinho(produto6);
+cliente1.adicionarProdutoNoCarrinho(produto6);
+cliente1.adicionarProdutoNoCarrinho(produto6);
+
 
 cliente1.gerarNF();
