6fb1fdfda57cae08b18674322ff19dcdcd9f9580
renan tomazini <renantomazini@gmail.com>
2022-05-23 16:22:45 -0300

    Exercicio do dia

---
+++ AlgebraLinear/questao1.js
@@ -0,0 +1,30 @@
+//Verifique se a aplicação de t r3->r2 definida por t(x,y,z) = (z,x+y) é uma transformação linear
+
+function transformacao(x,y,z){
+    return ([z, x+y]);
+}
+
+//provar que t(u+v) = t(u) + t(v)
+
+function somarTransformacao(tu,tv){
+    a = tu[0] + tv[0];
+    b = tu[1] + tv[1];
+    a.sort();
+    b.sort();
+    return([a,b]);
+}
+
+//t(u)
+tu = transformacao("x","y","z");
+
+//t(v)
+tv = transformacao("a","b","c");
+
+//t(u+v)
+tuv = transformacao("ax","by","zc");
+
+//veriricar se as saidas são iguais:
+console.log(tuv);
+console.log(somarTransformacao(tu,tv))
+
+
