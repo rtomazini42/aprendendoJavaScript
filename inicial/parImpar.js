ab5e44ed686ed278ebb38b3a0e461bedc24c6a79
renan tomazini <renantomazini@gmail.com>
2022-05-26 17:30:12 -0300

    Atividade do dia

---
+++ inicial/parImpar.js
@@ -0,0 +1,12 @@
+function parImpar(n){
+
+    if(n%2 == 0){
+        return "par";
+    }else{
+        return "impar";
+    }
+}
+
+console.log(parImpar(2));
+console.log(parImpar(3));
+console.log(parImpar(111));
